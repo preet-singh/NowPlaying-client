88031a7fe8b587c4a786c5c5ffcdf9a277bdd74c
Leon Dail <leondailofficial@gmail.com>
2019-12-02 12:18:17 -0800

    added context for category, made home page display

--- src/components/ThreadsList/ThreadsList.js
+++ src/components/ThreadsList/ThreadsList.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ThreadItem from '../ThreadItem/ThreadItem';
 import './ThreadsList.css';
+import AuthService from '../../utils/auth-service';
+import UserContext from '../../utils/context';
 
-function ThreadsList(props) {
-  return (
-    <div className='threads-list'>
-      <h3>Movies</h3>
-      <ThreadItem />
-      <ThreadItem />
-      <ThreadItem />
-    </div>
-  )
+async function getThreads() {
+  let threads = await AuthService.getSpecificThreads(this.context.category);
+  let returnItem = [];
+  for (let i=0;i<3;i++) {
+    returnItem.push(<ThreadItem details={threads[i]} key={i} />)
+  }
+  return returnItem;
+}
+class ThreadsList extends React.Component {
+  static contextType = UserContext;
+  render() {
+    return (
+      <div className='threads-list'>
+        <h3>{this.context.category}</h3>
+        {getThreads()}
+      </div>
+    )
+  }
 }
 
 export default ThreadsList;
