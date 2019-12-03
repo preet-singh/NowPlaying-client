84faf2e614c4f7b7e6adb6492c1df786a81a6886
Marlon Agno <mdj.agno@gmail.com>
2019-12-03 16:30:49 -0500

    changed login to update usercontext and conditionally render login header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,15 +7,18 @@ import { Link } from 'react-router-dom';
 import Login from '../../components/Login/Login';
 import LoginButton from '../../components/LoginButton/LoginButton';
 import './Header.css';
+import TokenService from '../../utils/token-service'
 
 //images
 
 function pathSpecificLogin(location) {
-  if (location.pathname === "/") {
-    return <Login />;
-  }
-  else if (location.pathname === "/register" || location.pathname === "/login") {
-    return;
+  if(!TokenService.hasAuthToken()){
+    if (location.pathname === "/") {
+      return <Login />;
+    }
+    else if (location.pathname === "/register" || location.pathname === "/login") {
+      return;
+    }
   }
   else {
     return <LoginButton />;
