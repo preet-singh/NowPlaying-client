6bafe8318f587f310e094cf5a26e4ee8ac7d2e5b
Marlon Agno <mdj.agno@gmail.com>
2019-12-03 16:46:13 -0500

    added public and private route components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {Switch, Route} from 'react-router-dom';
 import HomePageRoute from './routes/HomePage/HomePageRoute';
 import CategoryRoute from './routes/Category/CategoryRoute';
 import ThreadRoute from './routes/Thread/ThreadRoute';
-import LoginRoute from './routes/Login/LoginRoute';
+import LoginRoute from './routes/LoginRoute/LoginRoute';
 import RegisterRoute from './routes/Register/RegisterRoute';
 import NotFoundRoute from './routes/NotFound/NotFound';
-import UserContext from './utils/context'
+import UserContext from './utils/context';
 import AuthService from './utils/auth-service';
+import PublicOnlyRoute from './routes/PublicOnlyRoute/PublicOnlyRoute';
+import PrivateOnlyRoute from './routes/PrivateOnlyRoute/PrivateOnlyRoute';
 
 //EJ IS TESTING HERE TO PUSH
 
@@ -38,8 +40,8 @@ class App extends React.Component {
           <Route exact path="/" component={HomePageRoute} />
           <Route path="/category/:id" component={CategoryRoute} />
           <Route path="/:thread/:id" component={ThreadRoute} />
-          <Route path="/login" component={LoginRoute} />
-          <Route path="/register" component={RegisterRoute} />
+          <PublicOnlyRoute path="/login" component={LoginRoute} />
+          <PublicOnlyRoute path="/register" component={RegisterRoute} />
           <Route component={NotFoundRoute} />
         </Switch>
       </div>
