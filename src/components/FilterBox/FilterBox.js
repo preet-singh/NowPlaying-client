e188e4038ca36f263bba7472af9e7684ae2da913
preet-singh <preetsingh13@gmail.com>
2019-12-04 11:46:16 -0600

    Filtering in progress

--- src/components/FilterBox/FilterBox.js
+++ src/components/FilterBox/FilterBox.js
@@ -1,18 +1,77 @@
 //Dependencies
 import React, { Component } from 'react';
+import UserContext from '../../utils/context'
 
-class FilterBox extends Component() {
+class FilterBox extends Component {
+  static contextType = UserContext;
+
+  constructor(props){
+    super(props)
+    this.state = {
+      minimumYear: '',
+      maximumYear: '',
+      genre: '',
+      rating: '',
+      familyRating: '',
+      error: null,
+    }
+  }
+  
+  componentDidMount(){
+    this.context.setCategory(this.context.category)
+      .catch(res => {
+        this.setState({
+          error: res.error
+        })
+      })
+  }
+  
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render(){
+    let currentCategoryItems = this.context.categoryItems;
+
+    if(this.state.minimumYear){
+      currentCategoryItems = currentCategoryItems.filter(item => Number(this.state.minimumYear) <= Number(item.release_date.slice(0,4)))
+    }
+
+
     return (
       <form id="filterForm" name="filterForm">
         <label htmlFor="minimumYear" id="minimumYearLabel" name="minimumYearLabel">Earliest Year:</label>
-        <input type="text" placeholder="1920" id="minimumYear" name="minimumYear" />
+        <input 
+          type="text" 
+          placeholder="1920" 
+          id="minimumYear" 
+          name="minimumYear" 
+          onChange={this.handleChange} 
+        />
         <label htmlFor="latestYear" id="latestYearLabel" name="latestYearLabel">Latest Year:</label>
-        <input type="text" placeholder={new Date().getFullYear()} id="latestYear" name="latestYear" />
+        <input 
+          type="text" 
+          placeholder={new Date().getFullYear()}
+          id="latestYear"
+          name="maximumYear" 
+          onChange={this.handleChange} 
+        />
         <label htmlFor="genre" id="genreLabel" name="genreLabel">Genre:</label>
-        <input type="text" placeholder="Romance" />
+        <input 
+          type="text" 
+          placeholder="Romance"
+          name="genre"
+          onChange={this.handleChange}   
+        />
         <label htmlFor="rating" id="ratingLabel" name="ratingLabel">Minimum Rating:</label>
-        <select id="rating" name="rating" defaultValue="1">
+        <select 
+          id="rating" 
+          name="rating" 
+          defaultValue="1"
+          onChange={this.handleChange} 
+        >
           <option value="5">5</option>
           <option value="4">4</option>
           <option value="3">3</option>
@@ -20,7 +79,12 @@ class FilterBox extends Component() {
           <option value="1">1</option>
         </select>
         <label htmlFor="rating" id="ratingLabel" name="ratingLabel">Highest Family Rating:</label>
-        <select id="familyRating" name="familyRating" defaultValue="R">
+        <select 
+          id="familyRating" 
+          name="familyRating" 
+          defaultValue="R"
+          onChange={this.handleChange} 
+        >
           <option value="G">G</option>
           <option value="PG">PG</option>
           <option value="PG-13">PG-13</option>
