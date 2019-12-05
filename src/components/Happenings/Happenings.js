8b8dd3300ee8f1f8a93f06243346feecd2ef3fcc
William Bae <wbae912@gmail.com>
2019-12-05 14:04:28 -0800

    New comments and threads are showing. Working on only persisting 5 data points in the happenings table

--- src/components/Happenings/Happenings.js
+++ src/components/Happenings/Happenings.js
@@ -1,27 +1,67 @@
-//Dependencies
 import React, { Component } from 'react';
-
-//Style
+import config from '../../config';
 import './Happenings.css'
-
 
 class Happenings extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      comments: [],
+      error: null,
+      happenings: [],
       isLoading: false
     };
   }
-  render(){
+
+  componentDidMount() {
+    fetch(`${config.API_ENDPOINT}/happening`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .then(res => 
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    )
+    .then(data => {
+      this.setState({
+        happenings: data
+      })
+    })
+    .catch(res => {
+      this.setState({
+        error: res.error
+      })
+    })
+  }
+
+  render() {
+    let firstFourHappenings = this.state.happenings.slice(0,4);
+    let lastHappening = this.state.happenings[4] || {};
     return (
       <div className="Happenings">
         <h3>What's happening</h3>
         <ul className="happenings-list">
-          <li>Something here</li>
-          <li>Also something here</li>
-          <li>Last something here</li>
+          {firstFourHappenings.map(event =>
+              (event.media_title)
+               ? <>
+                  <li className="thread-created">A thread for the {event.media_type.slice(0,event.media_type.length - 1)}, <span className="media-title">{event.media_title}</span>, was created</li>
+                  <hr className="underline" />
+                 </>
+               : 
+                <>
+                 <li className="comment-created">{event.username} commented <span className="user-comment">"{event.user_comment}"</span> in the <span className="media-title">{event.media_title_comments}</span> thread</li>
+                 <hr className="underline" />
+                </>
+          )}
+          {(lastHappening.media_title)
+              ? 
+                <li className="thread-created">A thread for the {lastHappening.media_type.slice(0,lastHappening.media_type.length - 1)}, <span className="media-title">{lastHappening.media_title}</span>, was created</li>
+              : 
+                <li className="comment-created">{lastHappening.username} commented <span className="user-comment">"{lastHappening.user_comment}"</span> in the <span className="media-title">{lastHappening.media_title_comments}</span> thread</li> 
+          }
         </ul>
       </div>
     );
@@ -29,3 +69,19 @@ class Happenings extends Component {
 }
 
 export default Happenings;
+
+
+
+// Happenings table...it persists all data..we want it to have 5 items at a time
+
+// 1 2 3 4 5
+
+// Before we POST we NEED to check first of all IF there are 5 ITEMS IN OUR HAPPENINGS TABLE
+
+// IF THERE IS 5, WE REMOVE THE OLDEST ITEM IN THE DATABASE BASED ON TIMESTAMP USING THE DELETE REQUEST SO THAT IT PERSISTS IN DATABASE
+
+// NEED TO DELETE REQUEST IN OUR HAPPENINGS ROUTE 
+
+// AND THEN ONCE IT HAS BEEN DELETED, WE CALL THE POST REQUEST AND ADD THE NEW HAPPENING EVENT TO DATABASE
+
+// 1 2 3 4 5
