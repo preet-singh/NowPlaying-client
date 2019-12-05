dacfe67a4e5d8ed4bafca942e4c07d14f79d42d2
preet-singh <preetsingh13@gmail.com>
2019-12-05 14:32:58 -0600

    happening route started

--- src/components/Happenings/Happenings.js
+++ src/components/Happenings/Happenings.js
@@ -1,21 +1,31 @@
 //Dependencies
-import React from 'react';
+import React, { Component } from 'react';
 
 //Style
 import './Happenings.css'
 
 
-function Happenings() {
-  return(
-    <div className="Happenings">
-      <h3>What's happening</h3>
-      <ul className="happenings-list">
-        <li>Something here</li>
-        <li>Also something here</li>
-        <li>Last something here</li>
-      </ul>
-    </div>
-  );
+class Happenings extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      comments: [],
+      isLoading: false
+    };
+  }
+  render(){
+    return (
+      <div className="Happenings">
+        <h3>What's happening</h3>
+        <ul className="happenings-list">
+          <li>Something here</li>
+          <li>Also something here</li>
+          <li>Last something here</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Happenings;
