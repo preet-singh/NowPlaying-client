0e481d5977c238532ad95252269d0f620144871a
EJ132 <ejg132@gmail.com>
2019-12-10 12:12:30 -0800

    pagination is up and running

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Pagination.css'
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+    const pageNumbers = [];
+  
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+  
+    return (
+      <nav>
+        <ul className='pagination'>
+          {pageNumbers.map(number => (
+            <li key={number} className='page-item'>
+              <button onClick={() => paginate(number)} className='page-link'>
+                {number}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Pagination;
