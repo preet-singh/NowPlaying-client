42202ee242fb49db2000ed0bac9ef96cd59cedba
preet-singh <preetsingh13@gmail.com>
2019-12-11 16:30:49 -0600

    login box testing

--- src/components/LoginButton/LoginButton.test.js
+++ src/components/LoginButton/LoginButton.test.js
@@ -2,27 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginButton from './LoginButton';
 import { MemoryRouter } from 'react-router-dom';
-import renderer from 'react-test-renderer';
-// import { shallow } from 'enzyme';
-// import toJson from 'enzyme-to-json'
-
-
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json'
 
 describe('Login button testing', () => {
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<MemoryRouter><LoginButton /></MemoryRouter>, div);
+    ReactDOM.render(
+        <MemoryRouter>
+            <LoginButton />
+        </MemoryRouter>, 
+    div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('renders UI as expected', () => {
-    const tree = renderer
-        .create(
-            <MemoryRouter>
-                <LoginButton />
-            </MemoryRouter>)
-        .toJSON();
-        expect(tree).toMatchSnapshot();
-  })
-
-})
+  it('renders the playbutton', () => {
+    const wrapper = shallow(<LoginButton />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  });
+  
+});
