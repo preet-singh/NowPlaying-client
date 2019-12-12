e831f3e12cd5244d75fb934614f35aeb9476427b
preet-singh <preetsingh13@gmail.com>
2019-12-12 16:18:25 -0600

    quick fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://murmuring-lowlands-61113.herokuapp.com/api/',
+    API_ENDPOINT: 'https://murmuring-lowlands-61113.herokuapp.com/api',
     TOKEN_KEY: 'nowplaying-client-auth-token',
     API_KEY: '4ac5392bc939c102a89ae2c2fd81ed8d'
   }  
