148c68a39e6678e8b8e857114cc67c7bc82abae4
EJ132 <ejg132@gmail.com>
2019-12-12 13:30:51 -0800

    fixed login bug

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -2,27 +2,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+//Context
+import UserContext from '../../utils/context';
+
 //Style
 import './LoginButton.css'
 import TokenService from '../../utils/token-service';
 
-function LoginButton(props) {
-  if(TokenService.hasAuthToken()){
-    return (
+class LoginButton extends React.Component {
+  static contextType = UserContext;
+  render(){
+    return TokenService.hasAuthToken() ? 
+          <div className="LoginButton">
+                <Link to='/'><button type="button" id='logout_button' className={this.props.classNames} onClick={() => this.context.processLogout()}>
+              LOGOUT
+            </button></Link>
+          </div>
+
+    :
+    
         <div className="LoginButton">
-              <Link to='/'><button type="button" id='logout_button' className={props.classNames} onClick={() => TokenService.clearAuthToken()}>
-            LOGOUT
-          </button></Link>
+          <Link to="/login"><button type="button" className={this.props.classNames}>LOGIN</button></Link>
         </div>
-    )
-  }
-  
-  else{
-    return(
-      <div className="LoginButton">
-        <Link to="/login"><button type="button" className={props.classNames}>LOGIN</button></Link>
-      </div>
-    );
+
   }
 }
 
