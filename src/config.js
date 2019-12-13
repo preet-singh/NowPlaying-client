92391fac4df836b73a3608a78f0278e8c1c10715
preet-singh <preetsingh13@gmail.com>
2019-12-13 10:35:22 -0600

    updates to API key - now functional

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export default {
     API_ENDPOINT: 'https://murmuring-lowlands-61113.herokuapp.com/api',
     TOKEN_KEY: 'nowplaying-client-auth-token',
-    API_KEY: process.env.API_KEY,
+    API_KEY: process.env.REACT_APP_API_KEY
   }  
 
   //http://localhost:8000/api
+  // ||
