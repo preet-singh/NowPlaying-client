9b1ee21cf82043cbb86709c85ab8a892a8f0a733
Leon Dail <leondailofficial@gmail.com>
2019-12-13 09:29:47 -0800

    fixed back search issue

--- src/components/ThreadsList/ThreadsList.js
+++ src/components/ThreadsList/ThreadsList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ThreadItem from '../ThreadItem/ThreadItem';
 import './ThreadsList.css';
-import AuthService from '../../utils/auth-service';
 import UserContext from '../../utils/context';
 import {withRouter, Link} from 'react-router-dom';
 import Pagination from '../Pagination/Pagination.js'
@@ -20,8 +19,6 @@ class ThreadsList extends React.Component {
   getThreads = () => {
     let limit = this.props.limit || 5;
     let returnItem = [];
-    console.log(this.context.filteredCategoryItems)
-    console.log(this.context.categoryItems)
     if(this.context.filteredCategoryItems !== this.context.categoryItems){
       if(this.context.filteredCategoryItems.length === 0){
         return <p>No movies found! Click the create a new thread above to get started.</p>
@@ -32,8 +29,8 @@ class ThreadsList extends React.Component {
     } else {
 
     for (let i=0;i<=limit;i++) {
-      if (this.state.posts[i]) {
-        returnItem.push(<ThreadItem details={this.state.posts[i]} key={i} />)
+      if (this.context.filteredCategoryItems) {
+        returnItem.push(<ThreadItem details={this.context.filteredCategoryItems[i]} key={i} />)
       }
       else if (i === 0) {
         returnItem.push('No threads exist!');
@@ -71,7 +68,6 @@ class ThreadsList extends React.Component {
       const indexOfFirstPost = indexOfLastPost - postsPerPage;
       const currentPosts = allPosts.slice(indexOfFirstPost, indexOfLastPost);
 
-      console.log(currentPosts)
 
       if(!this.state.posts){
         this.setState({
@@ -108,5 +104,3 @@ class ThreadsList extends React.Component {
 }
 
 export default withRouter(ThreadsList);
-
-// <Pagination postsPerPage={this.state.postsPerPage} totalPosts={this.state.posts.length} paginate={this.paginate}/>
